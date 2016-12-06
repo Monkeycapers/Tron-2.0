00b76cea931cf644f7ef9d85399f7f0b039c738e
EVAN JESTY <S199753733@OCDSB>
2016-12-06 13:55:30 -0500

    Added the User class, implementing storing the User's information

--- Server/GameServer.java
+++ Server/GameServer.java
@@ -3,6 +3,10 @@ package Server;
 
 import Jesty.TCPBridge.ClientWorker;
 import Jesty.TCPBridge.Server;
+import org.json.JSONObject;
+import org.json.JSONWriter;
+
+import java.io.StringWriter;
 
 /**
  * Created by Evan on 10/21/2016.
@@ -18,16 +22,32 @@ public class GameServer extends Server {
 
     @Override
     public void onMessage(ClientWorker clientWorker, String message) {
+        User user = (User)(clientWorker.clientData);
+        JSONObject jsonObject = new JSONObject(message);
+        String argument = jsonObject.getString("argument");
+
+        if (argument.equals("...")) {
+            //...//
+            //To send back to client:
+            StringWriter stringWriter = new StringWriter();
+            JSONWriter jsonWriter = new JSONWriter(stringWriter)
+                    .object()
+                    .key("argument")
+                    .value("...")
+                    .endObject();
+            //
+        }
 
     }
 
     @Override
     public void onClose(ClientWorker clientWorker, int code) {
-
+        User user = (User)(clientWorker.clientData);
     }
+
     @Override
     public void onOpen(ClientWorker clientWorker, int code) {
-
+        clientWorker.clientData = new User();
     }
 
 }
---
+++ Server/User.java
@@ -0,0 +1,7 @@
+package Server;
+
+/**
+ * Created by S199753733 on 12/6/2016.
+ */
+public class User {
+}
